b6aad2b58cccede5c73bc712b47cc0017d2d23c9
Brennan Ward <Bward7864@gmail.com>
2019-11-12 16:39:29 -0500

    More fixes
    Hopefully fixes spontaneous teleportation into the middle of nowhere
    (moves position setting to before World#spawnEntity is called by using
    placeEntity).
    Also fixes how a safe portal location is determined (no more requirement
    of finding bedrock for upward transitions.

--- src/main/java/com/lothrazar/dimstack/transit/ActiveTransit.java
+++ src/main/java/com/lothrazar/dimstack/transit/ActiveTransit.java
@@ -16,46 +16,49 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockPos.MutableBlockPos;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.DimensionType;
-import net.minecraft.world.Teleporter;
+import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
+import net.minecraftforge.common.util.ITeleporter;
 
-public class ActiveTransit extends Teleporter {
+public class ActiveTransit implements ITeleporter {
 
+	protected WorldServer world;
 	protected PortalTile source;
 	protected Transit transit;
+	protected BlockPos portalPos;
 
 	public ActiveTransit(WorldServer world, PortalTile source, Transit transit) {
-		super(world);
+		this.world = world;
 		this.source = source;
 		this.transit = transit;
 	}
 
 	public void teleport(EntityPlayerMP player) {
 		if (transit.isRelative()) {
-			BlockPos position = createOrFindPortal();
+			portalPos = createOrFindPortal();
 			if (source.goesUpwards()) {
-				teleport(player, transit.getTargetDim(), position.getX() + 1.3D, position.getY() + 1, position.getZ() + 1.3D);
+				teleport(player, transit.getTargetDim(), portalPos.getX() + 1.3D, portalPos.getY() + 1, portalPos.getZ() + 1.3D);
 			} else {
-				teleport(player, transit.getTargetDim(), position.getX() + 0.5D, position.getY() + -3, position.getZ() + 0.5D);
+				teleport(player, transit.getTargetDim(), portalPos.getX() + 0.5D, portalPos.getY() + -3, portalPos.getZ() + 0.5D);
 			}
 			TextComponentTranslation to = new TextComponentTranslation("dimstack." + DimensionType.getById(transit.getTargetDim()).getName() + ".name");
 			player.sendMessage(new TextComponentTranslation("dimstack.teleport.info", to));
 		} else {
-			BlockPos pos = transit.getTargetPos();
+			portalPos = transit.getTargetPos();
 			for (int x = -1; x <= 1; x++) {
 				for (int z = -1; z <= 1; z++) {
-					world.setBlockState(pos.add(x, 0, z), Blocks.STONEBRICK.getDefaultState());
+					world.setBlockState(portalPos.add(x, 0, z), Blocks.STONEBRICK.getDefaultState());
 				}
 			}
 			for (int x = -1; x <= 1; x++) {
 				for (int z = -1; z <= 1; z++) {
 					for (int y = 1; y < 4; y++)
-						world.setBlockState(pos.add(x, y, z), Blocks.AIR.getDefaultState());
+						world.setBlockState(portalPos.add(x, y, z), Blocks.AIR.getDefaultState());
 				}
 			}
 			TextComponentTranslation to = new TextComponentTranslation("dimstack." + DimensionType.getById(transit.getTargetDim()).getName() + ".name");
 			player.sendMessage(new TextComponentTranslation("dimstack.teleport.info", to));
-			teleport(player, transit.getTargetDim(), pos.getX() + 0.5, pos.getY() + 1, pos.getZ() + 0.5);
+			teleport(player, transit.getTargetDim(), portalPos.getX() + 0.5, portalPos.getY() + 1, portalPos.getZ() + 0.5);
 		}
 	}
 
@@ -66,7 +69,6 @@ public class ActiveTransit extends Teleporter {
 		if (this.world != null && this.world.getMinecraftServer() != null && this.world.getMinecraftServer().getPlayerList() != null) {
 			PlayerList playerList = this.world.getMinecraftServer().getPlayerList();
 			playerList.transferPlayerToDimension(player, dimension, this);
-			player.setPositionAndUpdate(x, y, z);
 			this.world.playSound(null, x + 0.5D, y + 0.5D, z + 0.5D, SoundEvents.BLOCK_PORTAL_TRAVEL, SoundCategory.MASTER, 0.25F, this.world.rand.nextFloat() * 0.4F + 0.8F);
 		} else {
 			throw new IllegalArgumentException("Dimension: " + dimension + " doesn't exist, or the PlayerList was null.");
@@ -74,10 +76,18 @@ public class ActiveTransit extends Teleporter {
 	}
 
 	@Override
-	public void placeInPortal(Entity entityIn, float rotationYaw) {
-		entityIn.motionX = 0.0D;
-		entityIn.motionY = 0.0D;
-		entityIn.motionZ = 0.0D;
+	public void placeEntity(World world, Entity entity, float rotationYaw) {
+		if (transit.isRelative()) {
+			if (source.goesUpwards()) {
+				entity.setLocationAndAngles(portalPos.getX() + 1.3D, portalPos.getY() + 1, portalPos.getZ() + 1.3D, rotationYaw, 90);
+			} else {
+				entity.setLocationAndAngles(portalPos.getX() + 0.5D, portalPos.getY() + -3, portalPos.getZ() + 0.5D, rotationYaw, 90);
+			}
+		} else entity.setLocationAndAngles(portalPos.getX() + 0.5D, portalPos.getY() + 1, portalPos.getZ() + 0.5D, rotationYaw, 90);
+		entity.fallDistance = 0;
+		entity.motionX = 0.0D;
+		entity.motionY = 0.0D;
+		entity.motionZ = 0.0D;
 	}
 
 	/**
@@ -96,17 +106,16 @@ public class ActiveTransit extends Teleporter {
 			MutableBlockPos dest = new MutableBlockPos(x, y, z);
 
 			if (source.goesUpwards()) { //We just went up, so search upwards.
-				IBlockState state = world.getBlockState(dest);
 				while (true) {
-					if (y >= 255 || state.getBlock() == Blocks.BEDROCK && world.getBlockState(dest.setPos(x, y + 1, z)).getBlock() != Blocks.BEDROCK || state.getBlock() == DimstackMod.PORTAL) {
+					if (y >= 255 || world.getBlockState(dest.setPos(x, y + 1, z)).getBlock() != Blocks.BEDROCK) {
 						break;
 					}
-					state = world.getBlockState(dest.setPos(x, ++y, z));
+					y++;
 				}
 			} else { //We just went down, so search downwards.
 				IBlockState state = world.getBlockState(dest);
 				while (true) {
-					if (y <= 0 || state.getBlock() != Blocks.AIR && state.getBlock() != Blocks.BEDROCK || state.getBlock() == DimstackMod.PORTAL) {
+					if (y <= 0 || state.getBlock() != Blocks.AIR && state.getBlock() != Blocks.BEDROCK) {
 						break;
 					}
 					state = world.getBlockState(dest.setPos(x, --y, z));
