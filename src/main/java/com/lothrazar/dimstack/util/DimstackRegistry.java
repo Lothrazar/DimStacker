06a1927c53737a873ef9094f7ba9fa31f4f4e093
lothrazar <samson.bassett@gmail.com>
2021-12-03 14:35:23 -0800

    1.18 - ported dont release yet , gonna refactor some code

--- src/main/java/com/lothrazar/dimstack/util/DimstackRegistry.java
+++ src/main/java/com/lothrazar/dimstack/util/DimstackRegistry.java
@@ -10,7 +10,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.material.Material;
-import net.minecraftforge.fmllegacy.RegistryObject;
+import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
